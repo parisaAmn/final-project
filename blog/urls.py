c41c9c59c659a5c6817aa5b4fa2e4fc39ec60a42
parisaAmn <parisa.amani17@gmail.com>
2020-09-13 20:17:28 +0430

    set up frontend

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.sign_up, name='sign_up'),
+    path('', views.index, name='index'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('test/', views.test, name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', {})
+
 def sign_up(request):
     return render(request, 'blog/sign_up.html', {})
+
+
+def sign_in(request):
+    return render(request, 'blog/sign_in.html', {})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
+
+def contact(request):
+    return render(request, 'blog/contact.html', {})
+
+def test(request):
+    return render(request, 'blog/test.html', {})
