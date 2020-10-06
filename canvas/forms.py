2428e3d4c044894f7f73da9a03bb60d50fae0e50
parisaAmn <parisa.amani17@gmail.com>
2020-10-07 01:43:02 +0330

    create sign in & sign up forms

---
+++ canvas/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from .models import CanvasProfile
+
+class SignUpForm(UserCreationForm):
+    canvasfingerprint = forms.CharField(max_length=64)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'canvasfingerprint' )
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repeat your password'
+
+
+class SignInForm(AuthenticationForm):
+    canvasfingerprint = forms.CharField(max_length=64)
+
+    class Meta:
+        model = User
+        fields = ('username','password', 'canvasfingerprint' )
+    def __init__(self, *args, **kwargs):
+        super(SignInForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
+
