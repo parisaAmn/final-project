8d57baf1f05b862377078d8138c0ebfda0e0fdb1
parisaAmn <parisa.amani17@gmail.com>
2020-10-07 00:42:41 +0330

    create canvas model

--- canvas/models.py
+++ canvas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 # Create your models here.
+
+class CanvasProfile(User):
+    # username = models.CharField(max_length = 150)
+    # password = models.CharField(max_length = 150)
+    # email = models.EmailField()
+    canvasfingerprint = models.CharField(max_length=64)
+    uniqueness = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.username + ' / ' + str(self.email) +' / '+ self.canvasfingerprint+' / '+str(self.uniqueness)
