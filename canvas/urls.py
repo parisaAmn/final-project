19f653ff17a2054f1fdd0efde211d1d56cb770a7
parisaAmn <parisa.amani17@gmail.com>
2020-10-07 04:49:35 +0330

    Edit url names in canvas

--- canvas/urls.py
+++ canvas/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='cindex'),
-    path('signup/', views.signup, name='csignup'),
-    path('sign_in/', views.sign_in, name='csign_in'),
-    path("signout", views.signout, name="csignout"),
-    path('about/', views.about, name='cabout'),
-    path('contact/', views.contact, name='ccontact'),
-    path('test/', views.test, name='ctest'),
-    path('ajax/', views.ajaxx, name='cajax'),
+    path('index/', views.index, name='c_index'),
+    path('signup/', views.signup, name='c_signup'),
+    path('sign_in/', views.sign_in, name='c_sign_in'),
+    path("signout", views.signout, name="c_signout"),
+    path('about/', views.about, name='c_about'),
+    path('contact/', views.contact, name='c_contact'),
+    path('test/', views.test, name='c_test'),
+    path('ajax/', views.ajaxx, name='c_ajax'),
 
 ] 
